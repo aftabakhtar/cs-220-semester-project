ef72411f0fccaf6b745ecc72d274e677dfcf7a10
Usama Ahmed <usiddiquie.bscs18seecs@seecs.edu.pk>
2019-11-11 18:22:43 +0500

    Booking tables implemented.

--- sql_scripts/table_creations/bookings.sql
+++ sql_scripts/table_creations/bookings.sql
@@ -2,10 +2,17 @@ drop database if exists flight_service;
 create database flight_service;
 use flight_service;
 
+create table seat_type(
+    seat_type_id int primary key,
+    seat_type_name varchar(8)
+);
+
 create table seat(
 	seat_id varchar(2),
     plane_id int,
+    seat_type int,
     foreign key (plane_id) references plane(plane_id),
+    foreign key (seat_type) references seat_type(seat_type_id),
 	primary key (seat_id, plane_id)
 );
 
@@ -16,12 +23,24 @@ create table customer(
     freq_flier_points int
 );
 
+create table billing(
+    billing_id int primary key,
+    customer_id int,
+    amount int,
+    data date,
+    time time,
+    online_booking bool,
+    foreign key (customer_id) references customer(customer_id)
+);
+
 create table booking(
 	booking_id int primary key,
     flight_id int,
     customer_id int,
     seat_id varchar(2),
+    billing_id int,
     foreign key (flight_id) references flight(flight_id),
     foreign key (customer_id) references customer(customer_id),
-    foreign key (seat_id) references seat(seat_id)
+    foreign key (seat_id) references seat(seat_id),
+    foreign key (billing_id) references billing(billing_id)
 );
